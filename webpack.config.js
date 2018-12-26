d6c74c1e8e6741766eba62540edab88f4cd4e368
Francis Usher <francis.w.usher@gmail.com>
2018-12-26 04:14:14 -0800

    Setup webpack-dev-server to run through npm script, including static content.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+var path = require('path')
+
 module.exports = {
   entry: './src/game.js',
+  devServer: {
+    contentBase: [path.join(__dirname, 'dist'), path.join(__dirname, 'static')],
+    compress: true,
+    port: 9000,
+  },
   module: {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
