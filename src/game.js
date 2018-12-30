52232c4cc5e4ca65424e81e2ca14ecf40f77b5d6
Francis Usher <francis.w.usher@gmail.com>
2018-12-29 19:36:36 -0800

    Refactor parallax background implementation to reduce duplicated code.

--- src/game.js
+++ src/game.js
@@ -17,52 +17,91 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
-
 console.log(game)
+
+let scrollingBGs = [
+  {
+    name: 'cloud',
+    path: 'image/glacial-mountain/cloud_lonely.png',
+    velocity: -0.02,
+  },
+  {
+    name: 'clouds',
+    path: 'image/glacial-mountain/clouds_BG.png',
+    velocity: 0.04,
+  },
+]
+
+let scrollingBGs2 = [
+  {
+    name: 'cloudsMG3',
+    path: 'image/glacial-mountain/clouds_MG_3.png',
+    velocity: 0.1,
+  },
+  {
+    name: 'cloudsMG2',
+    path: 'image/glacial-mountain/clouds_MG_2.png',
+    velocity: -0.25,
+  },
+  {
+    name: 'cloudsMG1',
+    path: 'image/glacial-mountain/clouds_MG_1.png',
+    velocity: 0.5,
+  },
+]
 
 function preload() {
   this.load.image('sky', 'image/glacial-mountain/sky.png')
-  this.load.image('cloud', 'image/glacial-mountain/cloud_lonely.png')
-  this.load.image('clouds', 'image/glacial-mountain/clouds_BG.png')
-  this.load.image('cloudsMG3', 'image/glacial-mountain/clouds_MG_3.png')
-  this.load.image('cloudsMG2', 'image/glacial-mountain/clouds_MG_2.png')
-  this.load.image('cloudsMG1', 'image/glacial-mountain/clouds_MG_1.png')
+  scrollingBGs.map(bg => {
+    this.load.image(bg.name, bg.path)
+  })
   this.load.image('mountains', 'image/glacial-mountain/mountains.png')
+  scrollingBGs2.map(bg => {
+    this.load.image(bg.name, bg.path)
+  })
   this.load.image('title', 'image/CRUSADER.png')
 
   this.load.spritesheet('dude', 'image/dude.png', {
     frameWidth: 32,
     frameHeight: 48,
   })
-  this.load.audio('beat', 'audio/african-drum-jam.wav')
-  this.load.audio('grunts', 'audio/grunts.wav')
+
+  this.load.audio('beat', 'audio/african-drum-jam.mp3')
+  this.load.audio('grunts', 'audio/grunts.mp3')
 }
 
 let titleLogoImage
-let cloudsMG1_1, cloudsMG1_2
-function initializeBackground() {
-  const sky = this.add.image(400, 300, 'sky')
-  sky.displayHeight = 600
-  sky.displayWidth = 800
-  const clouds = this.add.image(400, 300, 'clouds')
-  clouds.displayHeight = 600
-  clouds.displayWidth = 800
-  const mountains = this.add.image(400, 300, 'mountains')
-  mountains.displayHeight = 600
-  mountains.displayWidth = 800
-  const cloudsMG3 = this.add.image(400, 300, 'cloudsMG3')
-  cloudsMG3.displayHeight = 600
-  cloudsMG3.displayWidth = 800
-  const cloudsMG2 = this.add.image(400, 300, 'cloudsMG2')
-  cloudsMG2.displayHeight = 600
-  cloudsMG2.displayWidth = 800
-  cloudsMG1_1 = this.add.image(400, 300, 'cloudsMG1')
-  cloudsMG1_1.displayHeight = 600
-  cloudsMG1_1.displayWidth = 800
-  cloudsMG1_2 = this.add.image(-399, 300, 'cloudsMG1')
-  cloudsMG1_2.displayHeight = 600
-  cloudsMG1_2.displayWidth = 800
 
+function initializeBackground() {
+  const initFullBG = (bgObj, secondary) => {
+    const bg = this.add.image(
+      secondary ? (bgObj.velocity > 0 ? -399 : 1199) : 400,
+      300,
+      bgObj.name
+    )
+    bg.displayWidth = 800
+    bg.displayHeight = 600
+    return bg
+  }
+  const initStaticBG = name => {
+    const bg = this.add.image(400, 300, name)
+    bg.displayWidth = 800
+    bg.displayHeight = 600
+    return bg
+  }
+  const initScrollingBG = bg => {
+    return [initFullBG(bg), initFullBG(bg, true)]
+  }
+  initStaticBG('sky')
+  scrollingBGs = scrollingBGs.map(bg => ({
+    ...bg,
+    refs: initScrollingBG(bg),
+  }))
+  initStaticBG('mountains')
+  scrollingBGs2 = scrollingBGs2.map(bg => ({
+    ...bg,
+    refs: initScrollingBG(bg),
+  }))
   titleLogoImage = this.add.image(0, 300, 'title')
 }
 
@@ -102,11 +141,20 @@ function update() {
   if (titleLogoImage.x < 403) {
     titleLogoImage.x = titleLogoImage.x + 4
   }
-  if (cloudsMG1_1.x < 1200) {
-    cloudsMG1_1.x = cloudsMG1_1.x + 0.4
-    cloudsMG1_2.x = cloudsMG1_2.x + 0.4
+  scrollingBGs.map(bg => {
+    scrollWrapBG(bg.refs[0], bg.refs[1], bg.velocity)
+  })
+  scrollingBGs2.map(bg => {
+    scrollWrapBG(bg.refs[0], bg.refs[1], bg.velocity)
+  })
+}
+
+function scrollWrapBG(bg1, bg2, increment) {
+  if (bg1.x < 1200 && bg1.x > -400) {
+    bg1.x = bg1.x + increment
+    bg2.x = bg2.x + increment
   } else {
-    cloudsMG1_1.x = 400
-    cloudsMG1_2.x = -399
+    bg1.x = 400
+    bg2.x = increment > 0 ? -399 : 1199
   }
 }
